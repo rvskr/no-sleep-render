5d6a614949bd6ea92096dcbe3a1650b414d21882
voskreseniev <roman.mayber@gmail.com>
2024-07-04 16:57:05 +0300

    +

--- ping_websites.py
+++ ping_websites.py
@@ -1,9 +1,12 @@
 import os
 import requests
 from dotenv import load_dotenv
+import schedule
+import time
 
 # Загрузка переменных окружения из файла .env
 load_dotenv()
+
 # Считываем список URL сайтов из переменной окружения
 site_urls_str = os.getenv('SITE_URLS')
 if site_urls_str:
@@ -23,6 +26,14 @@ def ping_website(url):
     except Exception as e:
         print(f'Error pinging {url}: {str(e)}')
 
-if __name__ == '__main__':
+def schedule_ping():
     for url in site_urls:
         ping_website(url)
+
+# Назначаем задачу пинга каждые 3 минуты
+schedule.every(1).minutes.do(schedule_ping)
+
+if __name__ == '__main__':
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
