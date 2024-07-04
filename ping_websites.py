06fef2eb175c8c94468e944562400e8633a0e63e
voskreseniev <roman.mayber@gmail.com>
2024-07-04 16:53:54 +0300

    +

---
+++ ping_websites.py
@@ -0,0 +1,28 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+# Загрузка переменных окружения из файла .env
+load_dotenv()
+# Считываем список URL сайтов из переменной окружения
+site_urls_str = os.getenv('SITE_URLS')
+if site_urls_str:
+    site_urls = [url.strip() for url in site_urls_str.split(',')]
+    print(f'Loaded SITE_URLS: {site_urls}')
+else:
+    print('Variable SITE_URLS is not set or empty. Please check your environment setup.')
+    exit()
+
+def ping_website(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(f'Successfully pinged {url}')
+        else:
+            print(f'Failed to ping {url}. Status code: {response.status_code}')
+    except Exception as e:
+        print(f'Error pinging {url}: {str(e)}')
+
+if __name__ == '__main__':
+    for url in site_urls:
+        ping_website(url)
