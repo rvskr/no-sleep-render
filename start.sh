4a77fa1d6badb82655f77ac7037e47f785fe50f7
voskreseniev <roman.mayber@gmail.com>
2024-07-16 07:26:40 +0300

    6

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gunicorn -w $(($(nproc) - 1)) -b 0.0.0.0:8000 wsgi:app
-
--- wsgi.py
+++
@@ -1,7 +0,0 @@
-from app import app
-import threading
-from app import periodic_check
-
-if __name__ == "__main__":
-    threading.Thread(target=periodic_check, daemon=True).start()
-    app.run(host='0.0.0.0', port=8000)
